a4c13cdc5aa389011a6b6e5fb49516b32ee9c225
ssbrandt <sierrasbrandt@gmail.com>
2021-06-08 15:18:36 -0400

    Add Listing model

--- auctions/models.py
+++ auctions/models.py
@@ -6,11 +6,18 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    pass
-    
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=20, decimal_places=2)
+    listing_image = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
 
-class Bid(models.Model):
-    pass
-
-class Comment(models.Model):
-    pass
+    def __str__(self):
+        return f'Listing {self.id}: {self.title}'
+#
+# class Bid(models.Model):
+#     pass
+#
+# class Comment(models.Model):
+#     pass
