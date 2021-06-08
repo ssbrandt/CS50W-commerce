539b0f97caed78cac2023a7307154dac9c576f6c
ssbrandt <sierrasbrandt@gmail.com>
2021-06-08 12:41:16 -0400

    Initial Commit

---
+++ auctions/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    pass
+    
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
