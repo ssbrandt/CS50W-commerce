9a287bcf5c127f31333b65698627ef7087a67efb
ssbrandt <sierrasbrandt@gmail.com>
2021-06-08 15:40:55 -0400

    Update listings model to make category, image optional

--- auctions/models.py
+++ auctions/models.py
@@ -9,9 +9,10 @@ class Listing(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=20, decimal_places=2)
-    listing_image = models.CharField(max_length=200)
-    category = models.CharField(max_length=100)
+    listing_image = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
     status = models.BooleanField(default=True)
+    # need to add relationship to User
 
     def __str__(self):
         return f'Listing {self.id}: {self.title}'
