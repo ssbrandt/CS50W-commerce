831850679057aa1ac38b76af80469f0447465473
ssbrandt <sierrasbrandt@gmail.com>
2021-06-09 13:33:14 -0400

    Create Single Listing page

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("createlisting", views.create_listing, name="create_listing"),
+    path('listing/<int:listing_id>', views.view_listing, name='listing')
 ]
