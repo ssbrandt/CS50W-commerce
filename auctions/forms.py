ddb511a6f77d495405a2fac10a1a2c5d36714fcc
ssbrandt <sierrasbrandt@gmail.com>
2021-06-09 19:41:53 -0400

    Create form for bid

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Listing
+from .models import Listing, Bid
 
 
 class ListingForm(forms.ModelForm):
@@ -7,3 +7,9 @@ class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
         exclude = ['status', 'creator']
+
+class BidForm(forms.ModelForm):
+
+    class Meta:
+        model = Bid
+        exclude = ['bidder', 'listing', 'winning_bid']
